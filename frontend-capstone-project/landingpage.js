1e78df122f43140c30174f14c1a3580220ce7de5
feranmi <feranmygit@gmail.com>
2024-06-11 23:25:45 -0700

    started working on capstone project landing page

--- frontend-capstone-project/landingpage.js
+++ frontend-capstone-project/landingpage.js
@@ -22,6 +22,9 @@ document.getElementById('textcontent1').innerHTML = textContent1;
 const textContent2 = `Upcoming Event: Business Plan Workshop`;
 document.getElementById('textcontent2').innerHTML = textContent2;
 
+const textContent3 = `Exclusive Offer: Upgrade to Premium for Pro Templates!`;
+document.getElementById('textcontent3').innerHTML = textContent3;
+
 const para1 = `Today`;
 const para2 = `Exciting news! 
 You can now print your meticulously crafted business plans in PDF
@@ -40,6 +43,13 @@ const para6 = `Join us tomorrow for an interactive workshop
 on creating killer business plans that attract investors!`;
 document.getElementById('para5').innerHTML = para5;
 document.getElementById('para6').innerHTML = para6;
+
+const para7 = `Next Week`;
+const para8 = `Upgrade to our Premium plan and 
+unlock a treasure trove of professionally designed templates 
+to elevate your business plans.`;
+document.getElementById('para7').innerHTML = para7;
+document.getElementById('para8').innerHTML = para8;
 
 
 
