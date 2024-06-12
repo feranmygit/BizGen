26f5ea6d166d73721ddb77ca2fc5a40caf9dd337
feranmi <feranmygit@gmail.com>
2024-06-11 17:38:50 -0700

    started working on capstone project landing page

--- frontend-capstone-project/landingpage.js
+++ frontend-capstone-project/landingpage.js
@@ -10,5 +10,22 @@ Customized Plans and PDF Printing.`;
 document.getElementById('textTop').innerHTML = get_message;
 document.getElementById('textPar').innerHTML = textParagh;
 
+const _bPost = `Latest Buzz`
+document.getElementById('blogPost').innerHTML = _bPost;
+
+const textContent = `New Feature Alert: PDF Printing Enabled!`;
+document.getElementById('textcontent').innerHTML = textContent;
+
+const para1 = `Today`;
+const para2 = `Exciting news! 
+You can now print your meticulously crafted business plans in PDF
+ format with a single click.`;
+document.getElementById('para1').innerHTML = para1;
+document.getElementById('para2').innerHTML = para2;
+
+
+
+
+
 
 
