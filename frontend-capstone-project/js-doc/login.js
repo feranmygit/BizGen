48cfd08e9f7804a8b79e10b829c5e8b6c60d6074
feranmi <feranmygit@gmail.com>
2024-06-17 11:33:38 -0700

    started working on capstone project landing page

---
+++ frontend-capstone-project/js-doc/login.js
@@ -0,0 +1,16 @@
+const _logUsers = {
+    card1: `Login to Your Account`,
+    card2: `Have an account, please enter your login details `,
+    card3: `Or`,
+    card4: `Login with Facebook`,
+    card5: `Login with Google`,
+    card6: `Remember Me`,
+    card7: `Forgot Password?`,
+};
+document.getElementById('logTitle').innerHTML = _logUsers.card1;
+document.getElementById('logPar').innerHTML = _logUsers.card2;
+document.getElementById('logPar1').innerHTML = _logUsers.card3;
+document.getElementById('faCont').innerHTML = _logUsers.card4;
+document.getElementById('faCont1').innerHTML = _logUsers.card5;
+document.getElementById('checkMe1').innerHTML = _logUsers.card6;
+document.getElementById('forpass1').innerHTML = _logUsers.card7;
