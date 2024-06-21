6998b76260b97585659bd854155b8ca523513305
feranmi <feranmygit@gmail.com>
2024-06-21 13:01:33 -0700

    started working on capstone project dashboard page

--- frontend-capstone-project/pages/js-doc/dashboard.js
+++ frontend-capstone-project/pages/js-doc/dashboard.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
     const hamburger = document.getElementById('hamburger');
     const coverColor = document.getElementById('coverColor');
+    const searcBtn = document.getElementById('searcBtn');
+    const overlay = document.getElementById('overlay');
 
     function togglecoverColor(){
             coverColor.classList.toggle('open');
             
     };
+    function myinputIcon(){
+        overlay.classList.toggle('propmt');
+    };
+    searcBtn.addEventListener('click', myinputIcon);
     hamburger.addEventListener('click', togglecoverColor);
 
 });
@@ -14,9 +20,12 @@ function myFunction(){
     coverColor.classList.toggle('open');
 }
 
-const searcBtn = document.getElementById('searcBtn');
-const search = document.getElementById('search');
+const inputIcon = document.getElementById('inputIcon');
+const overlay = document.getElementById('overlay');
 
 function myBtn(){
-    search.classList.toggle('show');
+    inputIcon.classList.toggle('show');
 } 
+// function myinputIcon(){
+//     overlay.classList.toggle('propmt')
+// }
