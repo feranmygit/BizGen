55b8cdd05ca28ecc20ea7a27ccedd9dc9f31c8fc
feranmi <feranmygit@gmail.com>
2024-06-21 04:57:53 -0700

    started working on capstone project dashboard page

--- frontend-capstone-project/pages/js-doc/dashboard.js
+++ frontend-capstone-project/pages/js-doc/dashboard.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const hamburger = document.getElementById('hamburger');
     const coverColor = document.getElementById('coverColor');
-    // const navContainer = document.getElementById('navContainer');
 
     function togglecoverColor(){
             coverColor.classList.toggle('open');
