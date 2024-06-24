f7259d91072eecc22c60ed835cb9f41df2e4baf7
feranmi <feranmygit@gmail.com>
2024-06-24 09:20:29 -0700

    started working on capstone project dashboard page

--- frontend-capstone-project/pages/js-doc/dashboard.js
+++ frontend-capstone-project/pages/js-doc/dashboard.js
@@ -36,11 +36,7 @@ document.addEventListener('DOMContentLoaded', function(){
     });
 });
 
-// Load user avatar if available
-const user = getUser(username);
-if (user && user.avatar) {
-  document.getElementById('userAvatar').src = user.avatar;
-}
+
 
 
 
