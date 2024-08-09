957b133810397aaaf16aa0015fc68c5db19d8055
feranmi <feranmygit@gmail.com>
2024-08-09 03:25:30 -0700

    I update theme containers preferences

--- frontend-capstone-project/pages/js-doc/theme.js
+++ frontend-capstone-project/pages/js-doc/theme.js
@@ -3,16 +3,24 @@ document.addEventListener('DOMContentLoaded', function() {
     function applyTheme(theme) {
       const root = document.documentElement;
       if (theme === 'dark') {
-        root.style.setProperty('--primary_dashboardbackground_greyish_color', 'hsl(0, 0%, 42%)');
-        root.style.setProperty('--primary_light_greyish_color', 'hsl(0, 0%, 15%)');
+        root.style.setProperty('--primary_dashboardbackground_greyish_color', '#222127');
+        root.style.setProperty('--primary_light_greyish_color', '#222127');
         root.style.setProperty('--primary_allBiz_color', '#fff');
-        root.style.setProperty('--primary_black_color', 'rgb(240, 240, 240)');
+        root.style.setProperty('--primary_black_color', '#d6d6d5');
         root.style.setProperty('--primary_setting_color', '#0083ca');
         root.style.setProperty('--primary_white_color', 'rgb(240, 240, 240)');
-        root.style.setProperty('--primary_whiteMainDash_color', 'hsl(0, 0%, 17%)');
+        root.style.setProperty('--primary_whiteMenu_color', '#2d323a');
+        root.style.setProperty('--primary_whitePage_color', '#fff');
+        root.style.setProperty('--paragraph_greyHeader_color', 'rgb(240, 240, 240)');
+        root.style.setProperty('--primary_whiteMainDash_color', '#26252c');
         root.style.setProperty('--primary_overlaybtn_color', 'rgb(247, 237, 237, 0.178)');
         root.style.setProperty('--main_primaryGenBtn_color', 'rgb(0,78, 152, 255)');
         root.style.setProperty('--primary_blackDonload_color', 'rgb(8, 8, 8)');
+        root.style.setProperty('--primary_sidebar_color', '#f6f6f6');
+        root.style.setProperty('--primary_searchArea_color', '#2d323a');
+        root.style.setProperty('--primary_textArea_color', '#51565d');
+        root.style.setProperty('--primary_textAreaBoxShadow_color', 'hsl(0, 0%, 25%)');
+        root.style.setProperty('--primary_light_grey_color', 'hsl(228, 12%, 20%)');
         // Add other properties for dark mode
       } else {
         root.style.setProperty('--primary_dashboardbackground_greyish_color', 'hsl(0, 0%, 92%)');
@@ -21,10 +29,18 @@ document.addEventListener('DOMContentLoaded', function() {
         root.style.setProperty('--primary_black_color', 'rgb(8, 8, 8)');
         root.style.setProperty('--primary_setting_color', '#126415ab');
         root.style.setProperty('--primary_white_color', 'rgb(8, 8, 8)');
+        root.style.setProperty('--primary_whiteMenu_color', '#fff');
+        root.style.setProperty('--primary_whitePage_color', 'rgb(8, 8, 8)');
+        root.style.setProperty('--paragraph_greyHeader_color', 'hsl(0, 0%, 49%');
         root.style.setProperty('--primary_whiteMainDash_color', '#fff');
         root.style.setProperty('--primary_overlaybtn_color', 'rgb(0, 0, 0, 0.1)');
         root.style.setProperty('--main_primaryGenBtn_color', 'hsl(0, 0%, 95%)');
         root.style.setProperty('--primary_blackDonload_color', '#fff');
+        root.style.setProperty('--primary_sidebar_color', 'hsl(0, 0%, 40%)');
+        root.style.setProperty('--primary_searchArea_color', '#fff');
+        root.style.setProperty('--primary_textArea_color', 'hsl(0, 0%, 40%)');
+        root.style.setProperty('--primary_textAreaBoxShadow_color', 'rgba(0, 0, 0, 0.2)');
+        root.style.setProperty('--primary_light_grey_color', 'hsl(228, 12%, 90%)');
         // Add other properties for light mode
       }
     }
